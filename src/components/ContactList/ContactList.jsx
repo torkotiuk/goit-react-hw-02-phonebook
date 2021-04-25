24e6376fcf4daf73f4588c1585e7e63f3bdd955e
Andrii Torkotiuk <torkotiuk@gmail.com>
2021-04-26 00:40:26 +0200

    final styles

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,17 +1,43 @@
 import styles from './ContactList.module.scss';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  },
+  container: {
+    width: '350px',
+  },
+  list: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    width: '300px',
+    border: '1px solid grey',
+    borderRadius: '4px',
+    padding: '10px',
+  },
+}));
 
 const ContactList = ({ items, onDeleteContact, children }) => {
+  const classes = useStyles();
   return (
-    <div>
+    <div className={classes.root}>
       <h3 className="title">ContactList</h3>
       {children}
       {items.map(contact => (
         <li key={contact.id} className={styles.ContactList__item}>
           <p>{contact.name}</p>
           <p>{contact.number}</p>
-          <button type="button" onClick={() => onDeleteContact(contact.id)}>
+          <Button
+            variant="contained"
+            type="button"
+            onClick={() => onDeleteContact(contact.id)}
+          >
             Delete
-          </button>
+          </Button>
         </li>
       ))}
     </div>
